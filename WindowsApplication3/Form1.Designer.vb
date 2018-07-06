86b5cd52f6f3601d5874f67e6efbb4bb84358cf5
Type_0_Dev <thinktech2go@gmail.com>
2018-07-06 02:20:50 -0400

    added base with dlc merge

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.debug = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'TextBox2
+        '
+        resources.ApplyResources(Me.TextBox2, "TextBox2")
+        Me.TextBox2.Name = "TextBox2"
+        '
+        'debug
+        '
+        resources.ApplyResources(Me.debug, "debug")
+        Me.debug.Name = "debug"
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        resources.ApplyResources(Me.ComboBox1, "ComboBox1")
+        Me.ComboBox1.Name = "ComboBox1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.TabStop = False
+        '
+        'Button3
+        '
+        resources.ApplyResources(Me.Button3, "Button3")
+        Me.Button3.Name = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ComboBox2)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        resources.ApplyResources(Me.GroupBox2, "GroupBox2")
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.TabStop = False
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        resources.ApplyResources(Me.ComboBox2, "ComboBox2")
+        Me.ComboBox2.Name = "ComboBox2"
+        '
+        'TextBox3
+        '
+        resources.ApplyResources(Me.TextBox3, "TextBox3")
+        Me.TextBox3.Name = "TextBox3"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.debug)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents debug As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+
+    Public Shared strcProgramName As String =
+        "Python.exe"
+    Public Shared strcFilePath As String =
+        "CDNSP.py"
+    Public Shared dlUpVer As String =
+        "0"
+    Public Shared dlDlcUpVer As String =
+        "0"
+
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim titleid As String
+        titleid = TextBox1.Text
+        Dim url As String = "http://switchbrew.org/index.php?search=" & titleid
+        System.Diagnostics.Process.Start(url)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ComboBox1.Items.Add("0")
+        ComboBox1.Items.Add("1")
+        ComboBox1.Items.Add("65536")
+        ComboBox1.Items.Add("131072")
+        ComboBox1.Items.Add("196608")
+        ComboBox1.Items.Add("262144")
+        ComboBox1.Items.Add("327680")
+        ComboBox1.Items.Add("393216")
+        ComboBox1.Items.Add("458752")
+        ComboBox1.Items.Add("524288")
+        ComboBox1.Items.Add("589824")
+        ComboBox1.Items.Add("655360")
+        ComboBox1.Items.Add("720896")
+        ComboBox1.Items.Add("786432")
+        ComboBox1.Items.Add("854968")
+        ComboBox1.Items.Add("917504")
+        ComboBox1.Items.Add("983040")
+        ComboBox1.Items.Add("1048576")
+        ComboBox1.Items.Add("justdlc")
+        ComboBox2.Items.Add("0")
+        ComboBox2.Items.Add("1")
+        ComboBox2.Items.Add("65536")
+        ComboBox2.Items.Add("131072")
+        ComboBox2.Items.Add("196608")
+        ComboBox2.Items.Add("262144")
+        ComboBox2.Items.Add("327680")
+        ComboBox2.Items.Add("393216")
+        ComboBox2.Items.Add("458752")
+        ComboBox2.Items.Add("524288")
+        ComboBox2.Items.Add("589824")
+        ComboBox2.Items.Add("655360")
+        ComboBox2.Items.Add("720896")
+        ComboBox2.Items.Add("786432")
+        ComboBox2.Items.Add("854968")
+        ComboBox2.Items.Add("917504")
+        ComboBox2.Items.Add("983040")
+        ComboBox2.Items.Add("1048576")
+    End Sub
+
+    Private Sub UpVerComboBox_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        Dim titleid As String
+        titleid = TextBox1.Text
+        Dim url As String = "https://track8.mixtape.moe/zesket.html" & titleid
+        System.Diagnostics.Process.Start(url)
+    End Sub
+
+    Public Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        dlUpVer = ComboBox1.SelectedItem.ToString()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim titleid As String
+        Dim ticketid As String
+        Dim dblProgTaskID As Double
+        Dim strPathName As String
+        titleid = TextBox1.Text
+        ticketid = TextBox2.Text
+
+
+
+        If TextBox2.Text = "" Then
+            strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer
+        Else
+            strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer & "-" & ticketid
+        End If
+        dblProgTaskID = Shell(strPathName, vbMaximizedFocus)
+
+
+        MsgBox(strPathName, MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub Button3_Click_2(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim titleid As String
+        Dim ticketid As String
+        Dim dlcid As String
+        Dim dblProgTaskID As Double
+        Dim strPathName As String
+        titleid = TextBox1.Text
+        ticketid = TextBox2.Text
+        dlcid = TextBox3.Text
+
+        strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer & "-" & ticketid & " " & dlcid & "-" & dlDlcUpVer
+        dblProgTaskID = Shell(strPathName, vbMaximizedFocus)
+        MsgBox(strPathName, MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        dlDlcUpVer = ComboBox2.SelectedItem.ToString()
+    End Sub
+End Class
