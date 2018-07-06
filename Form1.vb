035436904d09db77f22be5926e46bdb4dfde7fb2
Type_0_Dev <thinktech2go@gmail.com>
2018-07-05 23:36:47 -0400

    added combobox

--- Form1.vb
+++ Form1.vb
@@ -3,30 +3,16 @@
     Public Shared strcProgramName As String =
         "Python.exe"
     Public Shared strcFilePath As String =
-        "C:\CDNSP\CDNSP.py"
+        "CDNSP.py"
+    Public Shared dlUpVer As String =
+        "0"
 
     Private Sub Button2_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim titleid As String
-        Dim ticketid As String
-        Dim dlUpVer As String
-        Dim dblProgTaskID As Double
-        Dim strPathName As String
-        titleid = TextBox1.Text
-        ticketid = TextBox2.Text
-        dlUpVer = TextBox3.Text
-
-        If TextBox2.Text = "" Then
-            strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer
-        Else
-            strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer & "-" & ticketid
-        End If
-        dblProgTaskID = Shell(strPathName, vbMaximizedFocus)
-
-        '''MsgBox(strPathName, vbMaximizedFocus)'''
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+       
 
     End Sub
 
@@ -38,11 +24,25 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        If My.Computer.FileSystem.FileExists("C:\Program Files\Python37\Python.exe") Then
 
-        End If
-
-
+        ComboBox1.Items.Add("0")
+        ComboBox1.Items.Add("1")
+        ComboBox1.Items.Add("65536")
+        ComboBox1.Items.Add("131072")
+        ComboBox1.Items.Add("196608")
+        ComboBox1.Items.Add("262144")
+        ComboBox1.Items.Add("327680")
+        ComboBox1.Items.Add("393216")
+        ComboBox1.Items.Add("458752")
+        ComboBox1.Items.Add("524288")
+        ComboBox1.Items.Add("589824")
+        ComboBox1.Items.Add("655360")
+        ComboBox1.Items.Add("720896")
+        ComboBox1.Items.Add("786432")
+        ComboBox1.Items.Add("854968")
+        ComboBox1.Items.Add("917504")
+        ComboBox1.Items.Add("983040")
+        ComboBox1.Items.Add("1048576")
 
     End Sub
 
@@ -55,5 +55,30 @@
         titleid = TextBox1.Text
         Dim url As String = "https://track8.mixtape.moe/zesket.html" & titleid
         System.Diagnostics.Process.Start(url)
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim titleid As String
+        Dim ticketid As String
+        Dim dblProgTaskID As Double
+        Dim strPathName As String
+        titleid = TextBox1.Text
+        ticketid = TextBox2.Text
+
+
+
+        If TextBox2.Text = "" Then
+            strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer
+        Else
+            strPathName = strcProgramName & " " & strcFilePath & " " & "-r -g" & " " & titleid & "-" & dlUpVer & "-" & ticketid
+        End If
+        dblProgTaskID = Shell(strPathName, vbMaximizedFocus)
+
+
+        MsgBox(strPathName, MsgBoxStyle.Information)
+    End Sub
+
+    Public Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        dlUpVer = ComboBox1.SelectedItem.ToString()
     End Sub
 End Class
